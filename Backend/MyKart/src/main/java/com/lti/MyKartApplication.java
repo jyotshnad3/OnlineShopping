49e089705e0dcdc757e29a2e835b9f9ddec0ef5c
jyotshnad3 <jyotshnadammu@gmail.com>
2021-09-27 20:20:38 +0530

    First commit

---
+++ Backend/MyKart/src/main/java/com/lti/MyKartApplication.java
@@ -0,0 +1,14 @@
+package com.lti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyKartApplication {
+
+	public static void main(String[] args) {
+		System.out.print(false);
+		SpringApplication.run(MyKartApplication.class, args);
+	}
+
+}
