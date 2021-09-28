31a08c7489518d6dd34a0b6757a3f0eedbfa3be1
AnushkaSolanke <anushkasolanke41@gmail.com>
2021-09-28 17:44:12 +0530

    Added pojo of Compare

---
+++ Backend/MyKart/src/main/java/com/lti/pojo/Compare.java
@@ -0,0 +1,61 @@
+package com.lti.pojo;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tcompare")
+public class Compare 
+{
+  @Id
+  private int compareid;
+  private int productid;
+  private int userid;
+  
+  @ManyToOne
+	@JoinColumn(name ="productid")
+	private Product compareProducts;
+  
+  @ManyToOne
+	@JoinColumn(name = "userid")
+	private User compareUser;
+  
+  public int getCompareid() {
+	return compareid;
+}
+public void setCompareid(int compareid) {
+	this.compareid = compareid;
+}
+public int getProductid() {
+	return productid;
+}
+public void setProductid(int productid) {
+	this.productid = productid;
+}
+public int getUserid() {
+	return userid;
+}
+public void setUserid(int userid) {
+	this.userid = userid;
+}
+public Compare() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+public Compare(int compareid, int productid, int userid) {
+	super();
+	this.compareid = compareid;
+	this.productid = productid;
+	this.userid = userid;
+}
+@Override
+public String toString() {
+	return "Compare [compareid=" + compareid + ", productid=" + productid + ", userid=" + userid + "]";
+}
+  
+  
+}
