570b7328f2797bb4bacec95066e3273cf94625a4
Nilotpal-Roy <nilotpal.roy2012@gmail.com>
2021-09-29 11:48:39 +0530

    Controller added

---
+++ Backend/MyKart/src/main/java/com/lti/controller/MyRestController.java
@@ -0,0 +1,16 @@
+package com.lti.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lti.service.RetailerService;
+
+
+
+@RestController
+@RequestMapping("/MyKart/rest")
+public class MyRestController 
+{
+	
+}
